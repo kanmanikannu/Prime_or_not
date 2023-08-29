4573f19556debd4cd2ae4c3857036b27fd33cd9d
kanmanikannu <kanmanikannu117@gmail.com>
2023-08-29 21:04:13 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int number=scan.nextInt();
+        System.out.println(Prime.Findprime(number));
+
+
+    }
+}
+class Prime
+{
+   static String Findprime(int number)
+   {
+       int temporary=0;
+       for(int iterator = 2 ; iterator <= number / 2 ; iterator++)
+       {
+           if(number % iterator == 0)
+           {
+             temporary=1;
+             break;
+           }
+       }
+       if (temporary != 1)
+           return "The number is a Prime";
+       else
+           return "The number is not Prime";
+   }
+}
